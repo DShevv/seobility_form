cc082d0dcc47c66d0d46ce85f6dd61f1e565a45b
Dmitry Shevtsov <dshevtsov20@gmail.com>
2022-07-28 14:12:03 +0300

    init test-task

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.sass";
+import FeedbackForm from "./components/feedback-form/feedback-form";
+
+function App() {
+  return (
+    <div className="App">
+      <FeedbackForm />
+    </div>
+  );
+}
+
+export default App;
